24374b1153bb9a0eae35c4faf6ab9abc04b879b1
Boyan Yanchev <byanchev6@gmail.com>
2025-01-16 13:07:50 +0200

    Solved the week 2 challenge from Pen4o's Odyssey

---
+++ pen4o's-odyssey/week-2.js
@@ -0,0 +1,30 @@
+function correctDoor() {
+  const randomNumber = Math.random();
+  let doorNumber;
+
+  if (randomNumber >= 0 && randomNumber < 1/3) {
+    doorNumber = 1;
+  } else if (randomNumber >= 1/3 && randomNumber < 2/3) {
+    doorNumber = 2;
+  } else if (randomNumber >= 2/3 && randomNumber < 1) {
+    doorNumber = 3;
+  }
+  console.log(doorNumber) //Проверка на цифрата, за да се уверя, че следващият код ще проработи
+  return doorNumber;
+}
+
+function pen4oGuess(number) {
+  const doorNumber = correctDoor();
+
+  if (number > 3) {
+    console.log('Invalid choice. Please select Door 1, 2, or 3.');
+    return;
+  } else if (number === doorNumber) {
+    console.log(`Congratulations, Pen4o! You found the treasure behind Door ${doorNumber}!`);
+  } else {
+    console.log('A bucket of cold water splashes on you! Unlucky Pen4o!');
+  }
+}
+
+pen4oGuess(1);
+
